9b408cd74aead7bbeb290ed410f4cca1923191b8
Frqei <adrien.cs@live.fr>
2021-10-17 23:00:52 +0200

    adding test for spider

--- tests/test_spider.py
+++ tests/test_spider.py
@@ -1,4 +1,7 @@
+from unittest import mock
+from pytest_mock import mocker
 import pytest
+import utils.parser
 from utils.spider import Spider
 
 
@@ -14,3 +17,22 @@ def test_display_connections(capsys):
     captured = capsys.readouterr()
     assert captured.out == "Page A -> Page B\n"
 
+
+def test_visit(mocker):
+    mocker.patch('utils.parser.PageParser.run', return_value=["pageA", "pageB"])
+    spider = Spider()
+    spider.visit("http://adrien.fazio/", 1, 0)
+    assert spider.to_visit == [('pageA', 1), ('pageB', 1)]
+
+
+def test_crawl(capsys, mocker):
+    mocker.patch('utils.parser.PageParser.run', return_value=["pageA", "pageB"])
+    spider = Spider()
+    spider.crawl("http://adrien.fazio/", max_depth=1)
+    captured = capsys.readouterr()
+    assert captured.out == "Start crawling @ http://adrien.fazio\nhttp://adrien.fazio -> pageA\nhttp://adrien.fazio -> pageB\npageA -> pageA\npageA -> pageB\npageB -> pageA\npageB -> pageB\nThe spider has finished crawling the web at http://adrien.fazio\n"
+    assert isinstance(spider.visited, set)
+    assert list(spider.visited).sort() == ["http://adrien.fazio", "pageA", "PageB"].sort()
+    assert spider.links_connections == [('http://adrien.fazio', 'pageA'), ('http://adrien.fazio', 'pageB'),
+                                        ('pageA', 'pageA'), ('pageA', 'pageB'), ('pageB', 'pageA'),
+                                        ('pageB', 'pageB')]
